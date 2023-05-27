64c00f896c7cd047a31dd133b3d90bde5c7e3a60
Lucas Vieira <ls6182315@gmail.com>
2023-05-27 19:18:24 +0000

    anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,17 @@
+def bubble_sort(arr):
+    '''Return a list sorted'''
+    array_length = len(arr)
+    for i in range(array_length):
+        for j in range(0, array_length-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    '''Return a tuple with boolean'''
+    split_first_string = list(first_string.lower())
+    split_second_string = list(second_string.lower())
+    a = "".join(bubble_sort(split_first_string))
+    b = "".join(bubble_sort(split_second_string))
+    return (a, b, a == b)
