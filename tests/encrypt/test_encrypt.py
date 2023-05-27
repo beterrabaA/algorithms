c12436cce7b3e0820dbcf4c92d3b37d14ea44baf
Lucas Vieira <ls6182315@gmail.com>
2023-05-27 18:28:31 +0000

    test encrypt

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("hello world", -2) == "dlrow olleh"
+    assert encrypt_message("hello world", 2) == "dlrow oll_eh"
+    assert encrypt_message("hello world", 3) == "leh_dlrow ol"
+
+    with pytest.raises(TypeError):
+        encrypt_message("hello world", "2")
